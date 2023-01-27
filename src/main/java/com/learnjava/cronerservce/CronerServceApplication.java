a865086cb7893bb27c68f608faea8b37912d95dc
Aman Kumar <aman_cont19@olaelectric.com>
2023-01-28 02:15:08 +0530

    first commit

---
+++ src/main/java/com/learnjava/cronerservce/CronerServceApplication.java
@@ -0,0 +1,13 @@
+package com.learnjava.cronerservce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronerServceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronerServceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnjava/cronerservce/CronerServceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnjava.cronerservce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronerServceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
